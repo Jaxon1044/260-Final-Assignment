18f97cb08c41121475c9c6c0db2d5339e28cc300
Jaxon1044 <jfroderberg@zagmail.gonzaga.edu>
2025-05-09 22:50:04 -0700

    JUst added a unit test that checks two files after re0wrting them with the main function. We're in business, baby!

--- Main.cpp
+++ Main.cpp
@@ -136,14 +136,57 @@ int main( int argc, char *argv[] ) {
 
 TEST_CASE("First_Test") {
 
-
-
 	cout << "Running tests...";
 	
+	string morseStrings[91] = {"", "", "", "", "", "", "", "", "", "",  // 0 - 9    REMEMBER: it starts at 0!
+		"", "", "", "", "", "", "", "", "", "",	// 10 - 19
+		"", "", "", "", "", "", "", "", "", "",	// 20 - 29
+		"", "", "/", "", "", "", "", "", "", "",	// 30 - 39
+		"", "", "", "", "", "", "", 				// 40 - 46
+		"", 			// 47: SPACE!
+		"-----", 		// 48: 0
+		".----",			// 49: 1
+		"..---", 		// 50: 2
+		"...--", 		// 51: 3
+		"....-", 		// 52: 4
+		".....", 		// 53: 5
+		"-....", 		// 54: 6
+		"--...", 		// 55: 7
+		"---..", 		// 56: 8
+		"----.", 		// 57: 9
+		"", "", "", "", "", "", "",				// 58 - 64
+		".-",             // 65: A
+		"-...",			 // 66: B
+		"-.-.",			 // 67: C
+		"-..",			 // 68: D
+		".",				 // 69: E
+		"..-.",			 // 70: F
+		"--.",			 // 71: G
+		"....",			 // 72: H
+		"..",			 // 73: I
+		".---",			 // 74: J
+		"-.-",			 // 75: K
+		".-..",			 // 76: L
+		"--",			 // 77: M
+		"-.",			 // 78: N
+		"---",			 // 79: O
+		".--.",			 // 80: P
+		"--.-",			 // 81: Q
+		".-.",			 // 82: R
+		"...",			 // 83: S
+		"-",				 // 84: T
+		"..-",			 // 85: U
+		"...-",			 // 86: V
+		".--",			 // 87: W
+		"-..-",			 // 88: X
+		"-.--",			 // 89: Y
+		"--.."};			 // 90: Z
+
+	int typeConversion = 0;  //Indicates what form of conversion the user has chosen.
+	int charCount = 0;
+
 	ifstream inFile;
 	ofstream outFile;
-	inFile.open( "/testFiles/test1.txt" );
-	
 
 	SUBCASE( "First basic test") {
 		CHECK( "bob" == "bob" );
@@ -170,6 +213,21 @@ TEST_CASE("First_Test") {
 
 	SUBCASE( "Comparison test 2") {
 		REQUIRE_FALSE( compareFiles( "english.txt", "morse.txt" ) );
+	}
+
+	SUBCASE( "Convert and compare" ) {
+
+		// Open the files
+		inFile.open("english.txt");
+		outFile.open("output.txt");
+
+		// Convert the text to morse.
+		convertEnglish( inFile, outFile, morseStrings, charCount );
+
+		closeIOFiles( inFile, outFile );
+
+		// Once the file is converted, compare the outputted file to a template solution:
+		CHECK( compareFiles( "output.txt", "morse.txt" ) );
 	}
 }
 
